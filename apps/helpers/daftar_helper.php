a96f63e7a0bbc5db06c36aaf43ce026e3844552a
Bayu S. Ajie P. Putra <bayu.setya.ajie@gmail.com>
2022-10-27 13:51:12 +0700

    add helper for send whatsapp

--- apps/helpers/daftar_helper.php
+++ apps/helpers/daftar_helper.php
@@ -345,3 +345,35 @@ if (!function_exists('check_org_ip')) {
     return $org;
   }
 }
+
+if (!function_exists('sendWA')) {
+  function sendWA($number, $message)
+  {
+    $data = [
+      'api_key' => 'b2d95af932eedb4de92b3496f338aa5f97b36ae0',
+      'sender'  => '6285158779991',
+      'number'  => $number,
+      'message' => $message
+    ];
+    $curl = curl_init();
+    curl_setopt_array(
+      $curl,
+      array(
+        CURLOPT_URL => "https://waps.bayusapp.com/app/api/send-message",
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => "",
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "POST",
+        CURLOPT_POSTFIELDS => json_encode($data)
+      )
+    );
+
+    $response = curl_exec($curl);
+
+    curl_close($curl);
+    return $response;
+  }
+}
